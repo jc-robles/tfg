64111aed2b5b27eb1cd57723345660296a6af72c
Juan Carlos <juancarlos.robles@alumnos.ui1.es>
2023-08-07 09:35:28 +0200

    add exception controller

---
+++ src/main/java/com/imudataprocessor/controller/ExceptionHandlerController.java
@@ -0,0 +1,17 @@
+package com.imudataprocessor.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleAllExceptions(Exception ex) {
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
