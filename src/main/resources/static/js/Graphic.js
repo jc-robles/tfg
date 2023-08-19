e24838ff4a6b5c0f403f98eaaa28b80be6e3ba9f
Juan Carlos <juancarlos.robles@alumnos.ui1.es>
2023-08-19 09:05:54 +0200

    refactor Graphic.js

--- src/main/resources/static/js/Graphic.js
+++ src/main/resources/static/js/Graphic.js
@@ -1,41 +1,23 @@
 const charts = [];
 
+const green = 'rgb(75, 192, 192)'
+const purple = 'rgb(153, 102, 255)'
+const orange = 'rgb(255, 159, 64)'
+const pink = 'rgba(255, 99, 132, 0.2)'
+const colors = [green, purple, orange, pink]
+
 function clearAllGraphics() {
     charts.forEach(function(element) {
         element.destroy();
     })
 }
 
-function dataValue(label, data, color) {
-    return {
-        label: label,
-        data: data,
-        fill: false,
-        borderColor: color,
-        tension: 0,
-        borderWidth: 1
-    };
-}
-
 function newGraphic(idElement, dataLabels, data) {
-
-        const arrayRange = (start, stop, step) =>
-            Array.from(
-                { length: (stop - start) / step + 1 },
-                (value, index) => start + index * step
-            );
-
-        let color = ['rgb(75, 192, 192)', 'rgb(153, 102, 255)', 'rgb(255, 159, 64)', 'rgba(255, 99, 132, 0.2)']
-        let dataValues = [];
-        for (var i = 0; i < data.length; i++) {
-          dataValues.push(dataValue(dataLabels[i], data[i], color[i]));
-        }
-
-        let myChart = new Chart(document.getElementById(idElement), {
+        let newChart = new Chart(document.getElementById(idElement), {
         type: 'line',
         data: {
-            labels: arrayRange(1, data[0].length, 1).map(String),
-            datasets: dataValues
+            labels: generateDataset(1, data[0].length, 1).map(String),
+            datasets: generateValues(dataLabels, data)
         },
         options: {
           scales: {
@@ -47,6 +29,32 @@ function newGraphic(idElement, dataLabels, data) {
         }
         });
 
-        window.addEventListener('resize', function() {myChart.resize();});
-        charts.push(myChart)
+        window.addEventListener('resize', function() {newChart.resize();});
+        charts.push(newChart)
+}
+
+function generateDataset(start, stop, step) {
+    return Array.from(
+        { length: (stop - start) / step + 1 },
+        (value, index) => start + index * step
+    )
+}
+
+function generateValues(dataLabels, data) {
+    let dataValues = [];
+    data.forEach((element, index) => {
+        dataValues.push(dataValue(dataLabels[index], element, colors[index]));
+    });
+    return dataValues;
+}
+
+function dataValue(label, data, color) {
+    return {
+        label: label,
+        data: data,
+        fill: false,
+        borderColor: color,
+        tension: 0,
+        borderWidth: 1
+    };
 }
